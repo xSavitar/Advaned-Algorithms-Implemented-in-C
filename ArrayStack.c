7721dd1413b0f892eeef78208bb833095a44e712
Alangi Derick <alangiderick@gmail.com>
2014-10-23 13:07:46 +0100

    Modification of the code to calculate time during runtime. No user interaction, just machine code execution.

--- ArrayStack.c
+++ ArrayStack.c
@@ -29,37 +29,36 @@ int Top; //top of the stack
 //main function definition
 int 
 main(void){
-	Item item, size, i;
+	Item item, i;
 
 	//generation of random numbers using my seed value as the 
 	// system time for perfect runtime simulation
 	srand(time(NULL));
 	
 	printf("This is a program that simulates the behavior of Stack Data Structure.\n");
-	printf("Enter the size of the Stack: ");
-	scanf("%d", &size);
-	printf("\n\nInitialising the Stack...\n");
-	Initialise_Stack(size);
-	sleep(2);
+
+	printf("\nInitialising the Stack...\n");
+	Initialise_Stack(N);
+	sleep(1);
 	
 	printf("Pushing Elements to the Stack.\n");
-	for(i=0; i<size; i++){
+	for(i=0; i<N; i++){
 		if(!Stack_Full()){
 			Push_Item_To_Stack(rand() % 100);	
 		}
 	}
-	printf("Initial Content of the Stack is: ");
-	Print_Stack_Content(size);
+	printf("Initial Content of the Stack(%d items) is: ", N);
+	Print_Stack_Content(N);
 	
-	for(i=0; i<(size/2); i++){
+	for(i=0; i<(N/2); i++){
 		if(!Stack_Empty()){
 			Pop_Item_From_Stack();
 		}
 	}
-	printf("After Popping %d items, Stack now Contains: ", size/2);
+	printf("\nAfter Popping %d items, Stack now Contains: ", N/2);
 	
 	//printf the remaining items in the stack
-	Print_Stack_Content(size);	
+	Print_Stack_Content(N/2);	
 return 0;
 }
 
@@ -91,9 +90,9 @@ int Stack_Empty(void){
 }
 
 //Displaying the content of the stack
-void Print_Stack_Content(int N){
+void Print_Stack_Content(int n){
 	int i;
-	for(i = N - 1; i>=0; i--){
+	for(i = n - 1; i>=0; i--){
 		printf("%d ", Stack[i]);
 	}
 	printf("\n");
